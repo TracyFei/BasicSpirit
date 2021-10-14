9b48cdee8dd2c2fa0bee6e1a41b24c4491ebabb1
TracyFei <tracyfeiguiyu7@gmail.com>
2021-10-14 18:34:20 +1300

    Update details.php

--- details.php
+++ details.php
@@ -334,9 +334,7 @@ if(isset($_GET['pro_id'])){
                        <div class="box"><!-- box Begin -->
                            <h1 class="text-center"> <?php echo $pro_title; ?> </h1>
                            
-                           <?php add_cart(); ?>
-                           
-                           <form action="details.php?add_cart=<?php echo $product_id; ?>" class="form-horizontal" method="post"><!-- form-horizontal Begin -->
+                           <form class="form-horizontal" method="post"><!-- form-horizontal Begin -->
                                <div class="form-group"><!-- form-group Begin -->
                                    <label for="" class="col-md-5 control-label">Products Quantity</label>
                                    
@@ -402,10 +400,68 @@ if(isset($_GET['pro_id'])){
                                
                                ?>
                                
-                               <p class="text-center buttons"><button class="btn btn-primary i fa fa-shopping-cart"> Add to cart</button></p>
+                               <p class="text-center buttons"><button type="submit" name="add_cart" class="btn btn-primary i fa fa-shopping-cart"> Add to cart</button></p>
                                
                            </form><!-- form-horizontal Finish -->
                            
+                           <?php
+                           
+                           if(isset($_POST['add_cart'])){
+        
+                            $ip_add = getRealIpUser();
+                            $pro_id = $row_products['product_id'];
+                            
+                            $p_id = $pro_id;
+                            
+                            $product_qty = $_POST['product_qty'];
+                            
+                            $product_size = $_POST['product_size'];
+                            
+                            $check_product = "select * from cart where ip_add='$ip_add' AND p_id='$p_id'";
+                            
+                            $run_check = mysqli_query($conn,$check_product);
+                            
+                            if(mysqli_num_rows($run_check)>0){
+                                
+                                echo "<script>alert('This product has already added in cart')</script>";
+                                echo "<script>window.open('details.php?pro_id=$p_id','_self')</script>";
+                                
+                            }else{
+                    
+                                $get_price ="select * from products where product_id='$p_id'";
+                    
+                                $run_price = mysqli_query($conn,$get_price);
+                    
+                                $row_price = mysqli_fetch_array($run_price);
+                    
+                                $pro_price = $row_price['product_price'];
+                    
+                                $pro_sale = $row_price['product_sale'];
+                    
+                                $pro_label = $row_price['product_label'];
+                    
+                                if($pro_label == "sale"){
+                    
+                                    $product_price = $pro_sale;
+                    
+                                }else{
+                    
+                                    $product_price = $pro_price;
+                    
+                                }
+                                
+                                $query = "insert into cart (p_id,ip_add,qty,p_price,size) values ('$p_id','$ip_add','$product_qty','$product_price','$product_size')";
+                                
+                                $run_query = mysqli_query($conn,$query);
+                                
+                                echo "<script>window.open('product_url','_self')</script>";
+                                
+                            }
+                            
+                        }
+
+                           ?>
+
                        </div><!-- box Finish -->
                        
                        <div class="row" id="thumbs"><!-- row Begin -->
