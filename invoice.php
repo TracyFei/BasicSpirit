9302c289dd07a7af9acc3a525018dab1bbe7f9ad
TracyFei <tracyfeiguiyu7@gmail.com>
2022-01-07 16:23:09 +1300

    Editing invoice

--- invoice.php
+++ invoice.php
@@ -1,28 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Invoice</title>
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+<body>
+
+
 <?php 
 
     $active='Invoice';
-    include("includes/header.php");
-
 ?>
+<style>
+body {
+  background-color: white;
+}
+</style>
+<div class="container" style="font-family: Maiandra GD; text-align: center">
 
-<div class="container" style="background-color: white; text-align: center;">
-
-<h3>INVOICE from:</h3>
+<h1 style="font-size: 30px;">INVOICE from:</h1>
 
 <!-- WHY -->
 <div class="invoiceBox">
-<h1>Basic Spirit</h1> 
+<p style="font-size: 80px; padding-top: 15px">Basic Spirit</p> 
 </div>
 </div>
 
-<?php 
     
-    include("includes/footer.php");
-    
-?>
-    
-    <script src="js/jquery-331.min.js"></script>
-    <script src="js/bootstrap-337.min.js"></script>
+<script src="js/jquery-331.min.js"></script>
+<script src="js/bootstrap-337.min.js"></script>
 
 </body>
 </html>
